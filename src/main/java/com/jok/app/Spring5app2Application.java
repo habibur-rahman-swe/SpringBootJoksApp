6596e88bb70d0a0bc8bd5bb879e1d5ee2ffd38d1
Habibur Rahman <habibur0199mhr@gmail.com>
2023-11-10 09:28:43 +0600

    Initial Commite

---
+++ src/main/java/com/jok/app/Spring5app2Application.java
@@ -0,0 +1,13 @@
+package com.jok.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5app2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5app2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jok/app/Spring5app2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jok.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5app2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
